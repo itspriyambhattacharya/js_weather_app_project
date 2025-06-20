a68566f5a368f4bf194b331f5367a668f4f32a66
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-20 11:46:47 +0530

    sending max temp, min temp, humidity and wind speed to HTML

--- app.js
+++ app.js
@@ -18,10 +18,14 @@ async function fetchForecast() {
     tempSpan.innerHTML = temp;
     const feels_like_temp = data.main.feels_like; // fetching feels like temparature
     const max_temp = data.main.max_temp; // fetching max temparature
+    maxTempSpan.innerHTML = max_temp;
     const min_temp = data.main.min_temp; // fetching min temparature
+    minTempSpan.innerHTML = min_temp;
     const humidity = data.main.humidity; // fetching humidity
+    humiditySpan.innerHTML = humidity;
 
     const wind_speed = data.wind.speed; // fetching wind speed
+    wsSpan.innerHTML = wind_speed;
 
     const cityName = data.name; // detching city name
     spanCity.innerHTML = cityName;
@@ -35,6 +39,8 @@ let cityVal = undefined;
 document.querySelector("#check-weather").addEventListener("click", (e) => {
   cityVal = cityInp.value;
   console.log(cityVal);
+  // Call the function
+  fetchForecast();
 });
 
 const spanCity = document.querySelector("#city"); // city span from HTML
@@ -43,6 +49,3 @@ const maxTempSpan = document.querySelector("#max-temp-val"); // max temp span
 const minTempSpan = document.querySelector("#min-temp-val"); // min temp span
 const humiditySpan = document.querySelector("#humidity-val"); // humidity span
 const wsSpan = document.querySelector("#wind-speed-val"); // wind speed span
-
-// Call the function
-// fetchForecast();
